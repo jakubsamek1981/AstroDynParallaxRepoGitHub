8594a1a5131c3f71b7832831bcf91c4c1314e31d
Jakub Samek <jakubsamek81@gmail.com>
2025-05-11 16:01:48 +0200

    second commit

--- src/AstroDynParallax.c
+++ src/AstroDynParallax.c
@@ -1,19 +1,33 @@
 /*
  ============================================================================
  Name        : AstroDynParallax.c
- Author      : 
- Version     :
+ Author      : Jakub Samek
+ Version     : 2.0
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Program calculate Binary star parameters with specified accuracy
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include "AstroDynParallax.h"
 
 int main(void) {
+
+
+
   double previousValue = 1.0, currentValue = 2.0;
   double accuracy_threshold;
+
+  float a = 4.5;
+  float b = 3.4;
+  float h;
+
+  h = sqrt(pow(a, 2) - pow(b, 2));
+  printf("Result: %.4f\n", h);
+
 
   printf("Enter a accuracy threshold in range (0.1 - 100.0): ");
   // Checking if the input is a valid floating-point number
@@ -22,7 +36,7 @@ int main(void) {
     return 1;  // Exit with error code
   }
 
-
+  printf("The mass of the Sun is approximately %.3e kg.\n", CONST_SUN_MASS);
 
   while (fabs((currentValue - previousValue) / currentValue) > accuracy_threshold) {
     previousValue = currentValue;
