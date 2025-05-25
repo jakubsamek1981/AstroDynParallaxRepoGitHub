d5b6ef73bc07c32906f69239dbc16f17ca421066
Jakub Samek <jakubsamek81@gmail.com>
2025-05-26 01:02:46 +0200

    fourth commit

--- src/AstroDynParallax.c
+++ src/AstroDynParallax.c
@@ -2,12 +2,13 @@
  ============================================================================
  Name        : AstroDynParallax.c
  Author      : Jakub Samek
- Version     : 3.0
+ Version     : 4.0
  Copyright   : Your copyright notice
  Description : Program calculate Binary star parameters with specified accuracy
                - input type and range check
                - structure reorder (.h file)
                - temp_converg function introduction shows correctness of while cycle
+               - print_output function introduction
  ============================================================================
  */
 
@@ -25,68 +26,135 @@ int main(void) {
   float m1;
   float m2;
   float accuracy_threshold;
+  float area_part;
+  float area_full;
+  float time_cycle;
+  float a_au;
+  float a_degrees;
+  float a_radians;
+  DynParallaxResults dyn_params;
+
+  float time_observation = 11;
 
   float m1_prev = 1;
   float m2_prev = 1;
 
-  double previousValue = 1.0, currentValue = 2.0;
-
-
   float a = 4.5;
   float b = 3.4;
-
-  int iteration = 0;
 
   printf("The mass of the Sun is approximately %.3e kg.\n", CONST_SUN_MASS);
 
   h = sqrt(pow(a, 2) - pow(b, 2));
   printf("Eccentricity: %.4f\n", h);
 
+  // Compute area part
+  area_part = a*b*(acos(h / a) - ((h / pow(a, 2)) * sqrt(pow(a, 2) - pow(h, 2))));
+  printf("Area part: %.4f\n", area_part);
 
-  printf("Enter a accuracy threshold in range (0.1 - 100.0): ");
+  // Compute area full
+  area_full = a*b*M_PI;
+  printf("Area full: %.4f\n", area_full);
 
+  // Compute area full
+  time_cycle = time_observation*area_full/area_part;
+  printf("Time of one cycle: %.4f\n", time_cycle);
+
+  // Compute semi-major-axis
+  a_au = pow((2*pow(time_cycle, 2)), (1.0/3.0));
+  printf("a_au: %.4f [au]\n", a_au);
+
+  printf("Enter a accuracy threshold in range (0.01 - 100.0): ");
   // Checking if the input is a valid floating-point number
-  if (scanf("%f", &accuracy_threshold) != 1) {
-    printf("Invalid input! Please enter a valid floating-point number.\n");
-    return 1;  // Exit with error code
+  if (scanf("%f", &accuracy_threshold) == 1) {
+    if (accuracy_threshold >= 0.01 && accuracy_threshold <= 100.0) {
+  	}
+    else
+    {
+	  printf("Invalid input: %.2f is out of range.\n", accuracy_threshold);
+	  return 1;  // Exit with error code
+	}
   }
-  // Checking the input is in range 0.1 - 100.0
-  if (accuracy_threshold < 0.1f && accuracy_threshold > 100.0f)
+  else
   {
-    printf("Accuracy threshold is out of range 0.1 - 100.0 \n");
+    printf("Invalid input! Please enter a valid floating-point number.\n");
+	return 1;  // Exit with error code
   }
 
-  m1 = temp_converg (m1_prev, accuracy_threshold);
+  dyn_params.a = a;
+  dyn_params.a_au = a_au;
+  dyn_params.time_cycle = time_cycle;
+  dyn_params.mag1_rel = 3.9;
+  dyn_params.mag2_rel = 5.3;
+  dyn_params.mass_sum = 2;
+  dyn_params.itteration = 0;
 
-  accuracy = accuracy_calculation (m1, 5.5, m1_prev, 2.2);
+  dyn_params = calc_dyn_parallax (dyn_params);
+
+  accuracy = accuracy_calculation (dyn_params.mass_sum, dyn_params.mass_sum_prev);
   printf("Accuracy calc: %lf\n", accuracy);
 
   while (accuracy >= accuracy_threshold) {
 
-	iteration++;
+    dyn_params = calc_dyn_parallax (dyn_params);
 
-	m1_prev = m1;
-    m1 = temp_converg (m1_prev, accuracy_threshold);
-
-    accuracy = accuracy_calculation (m1, 5.5, m1_prev, 2.2);
-    printf("Iteration %d: Accuracy = %f\n", iteration, accuracy);
+    accuracy = accuracy_calculation (dyn_params.mass_sum, dyn_params.mass_sum_prev);
+    printf("Accuracy = %f\n", accuracy);
 
   }
 
-  printf("Accuracy %f achieved in Iterations %d \n", accuracy, iteration);
+  printf("Accuracy %f achieved in Iterations %d \n", accuracy, dyn_params.itteration);
 
   return 0;
 }
 
-float accuracy_calculation (float m1, float m2, float m1_prev, float m2_prev)
+//float accuracy_calculation (float m1, float m2, float m1_prev, float m2_prev)
+float accuracy_calculation (float m, float m_prev)
 {
 	float accuracy;
-	accuracy = 100*fabs((m1_prev - m1) / m1_prev);
+	accuracy = 100*fabs((m_prev - m) / m_prev);
 	return accuracy;
 }
 
-float temp_converg (float m, float accuracy_threshold)
+DynParallaxResults calc_dyn_parallax (DynParallaxResults dyn_params)
 {
-	m = (m * 0.9) + (accuracy_threshold*0.5);
-	return m;
+  dyn_params.itteration++;
+
+  dyn_params.a_degrees = ((dyn_params.a/60.0)/60.0);
+  dyn_params.a_radians = dyn_params.a_degrees*M_PI/180.0;
+
+  dyn_params.distance_au = dyn_params.a_au/(tan(dyn_params.a_radians));
+  dyn_params.distance_pc = dyn_params.distance_au*4.8*pow(10, -6);
+  //printf("Distance: %lf [pc]\n", dyn_params.distance_pc);
+
+  dyn_params.mag1_abs = dyn_params.mag1_rel + 5 - (5 * log10(dyn_params.distance_pc));
+  dyn_params.mag2_abs = dyn_params.mag2_rel + 5 - (5 * log10(dyn_params.distance_pc));
+  //printf("mag1_abs: %lf \n", dyn_params.mag1_abs);
+  //printf("mag2_abs: %lf \n", dyn_params.mag2_abs);
+
+  dyn_params.L1 = CONST_SUN_LUMINOSITY*pow(10, ((dyn_params.mag1_abs - CONST_SUN_MAGNITUDE))/-2.5);
+  dyn_params.L2 = CONST_SUN_LUMINOSITY*pow(10, ((dyn_params.mag2_abs - CONST_SUN_MAGNITUDE))/-2.5);
+  //printf("L1: %.4e [W]\n", dyn_params.L1);
+  //printf("L2: %.4e [W]\n", dyn_params.L2);
+
+  dyn_params.mass1 = CONST_SUN_MASS*pow((dyn_params.L1/CONST_SUN_LUMINOSITY), (1.0/3.5));
+  dyn_params.mass2 = CONST_SUN_MASS*pow((dyn_params.L2/CONST_SUN_LUMINOSITY), (1.0/3.5));
+  //printf("mass1: %.4e \n", dyn_params.mass1);
+  //printf("mass2: %.4e \n", dyn_params.mass2);
+
+  dyn_params.mass_sum_prev = dyn_params.mass_sum;
+  dyn_params.mass_sum = (dyn_params.mass1/CONST_SUN_MASS) + (dyn_params.mass2/CONST_SUN_MASS);
+  //printf("mass_summ: %f \n", dyn_params.mass_sum);
+
+  print_output (dyn_params);
+
+  dyn_params.a_au = pow((dyn_params.mass_sum*(pow(dyn_params.time_cycle,2.0))),(1.0/3.0));
+
+  return dyn_params;
 }
+
+void print_output (DynParallaxResults dyn_params)
+{
+  printf("----------------------------------------------------------------------------------------- \n");
+  printf("iteration = %i; a_au = %f[au]; distance = %f[pc]; mag1_abs = %.4f; mag2_abs = %.4f; mass1 = %.4e; mass2 = %.4e; \n", dyn_params.itteration, dyn_params.a_au, dyn_params.distance_pc, dyn_params.mag1_abs, dyn_params.mag2_abs, dyn_params.mass1, dyn_params.mass2);
+}
+
--- src/AstroDynParallax.h
+++ src/AstroDynParallax.h
@@ -4,10 +4,35 @@
 
 // Constants definition
 #define CONST_SUN_MASS 1.989E30
+#define CONST_SUN_MAGNITUDE 4.9E0
 #define CONST_SUN_LUMINOSITY 3.827E26
 
+// Define a structure to hold multiple return values
+typedef struct {
+	float a;
+	float a_degrees;
+	float a_radians;
+	float a_au;
+	float time_cycle;
+	float distance_au;
+	float distance_pc;
+	float mag1_abs;
+	float mag2_abs;
+	float L1;
+	float L2;
+	float mass1;
+	float mass2;
+	float mag1_rel;
+	float mag2_rel;
+	float mass_sum;
+	float mass_sum_prev;
+	int itteration;
+} DynParallaxResults;
+
 // Functions prototype
-float accuracy_calculation (float m1, float m2, float m1_prev, float m2_prev);
-float temp_converg (float m1, float accuracy_threshold);
+//float accuracy_calculation (float m1, float m2, float m1_prev, float m2_prev);
+float accuracy_calculation (float m, float m_prev);
+void print_output (DynParallaxResults dyn_params);
+DynParallaxResults calc_dyn_parallax (DynParallaxResults dyn_params);
 
 #endif
